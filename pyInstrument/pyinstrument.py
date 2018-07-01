cff3180cb23bcce74116eea6e6d8fe5102a33a1f
Ross Wilson <rzzzwilson@gmail.com>
2018-07-01 17:30:42 +0700

    Faffing around trying to recognize serial port from USB device.

--- pyInstrument/pyinstrument.py
+++ pyInstrument/pyinstrument.py
@@ -4,30 +4,30 @@
 A small library to allow a python program control the DigitalVFO.
 """
 
+import sys
 import usb.core
 import usb.util
-import sys
+import serial
 
 # Teensy USB identifiers
 VendorID = 0x16c0
 ProductID = 0x0483
 
 ######
-# Code to find all Teensy devices
+# Code to find all Teensy devices that can communicate
 ######
 
-class find_class(object):
-
-    def __init__(self, vendor_id, product_id):
-        self.vendor_id = vendor_id
-        self.product_id = product_id
-
-    def __call__(self, device):
-        return (device.idVendor == self.vendor_id
-                and device.idProduct == self.product_id)
-
 def find_teensy():
-   return usb.core.find(find_all=True, custom_match=find_class(VendorID, ProductID))
+    """Returns a list of all Teensy devices."""
+
+    # find USB devices
+    devices = usb.core.find(find_all=True)
+    # loop through devices, printing vendor and product ids in decimal and hex
+    result = []
+    for dev in devices:
+        if dev.idVendor == VendorID and dev.idProduct == ProductID:
+            result.append(dev)
+    return result
 
 
 devices = find_teensy()
@@ -36,3 +36,4 @@ print(f'{len(list(devices))} teensy devices found')
 for x in devices:
     print(f'product={x.product}, manufacturer={x.manufacturer}')
     print(x._get_full_descriptor_str())
+    print("")
--- pyInstrument/test.py
+++ pyInstrument/test.py
@@ -6,9 +6,6 @@ import usb.core
 dev = usb.core.find(find_all=True)
 # loop through devices, printing vendor and product ids in decimal and hex
 for cfg in dev:
-#  sys.stdout.write('Decimal VendorID=' + str(cfg.idVendor) + ' & ProductID=' + str(cfg.idProduct) + '\n')
   print(f'product={cfg.product}, manufacturer={cfg.manufacturer}')
-#  print(str(dir(cfg)))
   print(cfg._get_full_descriptor_str())
-  sys.exit(0)
-#  sys.stdout.write('Hexadecimal VendorID=' + hex(cfg.idVendor) + ' & ProductID=' + hex(cfg.idProduct) + '\n\n')
+#  sys.exit(0)
